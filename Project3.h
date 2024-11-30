ef6148efd6e2feaed07046b46ecf9facbe3cda97
yahneyn <116751533+yahneyn@users.noreply.github.com>
2024-11-30 12:42:57 -0500

    template for weightedScoring algorithm

--- Project3.h
+++ Project3.h
@@ -1,1 +1,30 @@
+#pragma once
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
 
+
+string weightedScoring(map<int, vector<string>> cityList, string state) {
+  //weight certain aspects more than others
+  pair<string, int> highScore = makepair(name, 0);
+  for(auto iter = cityList.begin(); iter != cityList.end(); iter++) {
+    int sum = 0;
+    for(int i = 0; i < iter.second.size(); i++ {
+      //weight * aspect
+      if(isdigit(iter.second[i][0]) {
+        sum += stoi(iter.second[i]);
+      }
+      else{
+        if(iter.second[i] == state) {
+          sum ++;
+        }
+      }
+    }
+    if(sum > highScore.second) {
+      highScore.first = iter.first;
+      highScore.second = sum;
+    }
+  }
+  return highScore.first;
+}
