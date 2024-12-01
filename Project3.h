68d5379ad122faa350b87f514c3425288a4bcddb
yahneyn <116751533+yahneyn@users.noreply.github.com>
2024-11-30 20:46:17 -0500

    Update Project3.h

--- Project3.h
+++ Project3.h
@@ -6,25 +6,24 @@ using namespace std;
 
 
 string weightedScoring(map<int, vector<string>> cityList, string state) {
-  //weight certain aspects more than others
-  pair<string, int> highScore = makepair(name, 0);
-  for(auto iter = cityList.begin(); iter != cityList.end(); iter++) {
-    int sum = 0;
-    for(int i = 0; i < iter.second.size(); i++ {
-      //weight * aspect
-      if(isdigit(iter.second[i][0]) {
-        sum += stoi(iter.second[i]);
-      }
-      else{
-        if(iter.second[i] == state) {
-          sum ++;
+    //weight certain aspects more than others
+    pair<string, int> highScore = make_pair("name", 0);
+    for(auto iter = cityList.begin(); iter != cityList.end(); iter++) {
+        int sum = 0;
+        for (int i = 0; i < iter->second.size(); i++) {
+            //weight * aspect
+            if (isdigit(iter->second[i][0])) {
+                sum += stoi(iter->second[i]);
+            } else {
+                if (iter->second[i] == state) {
+                    sum++;
+                }
+            }
         }
-      }
+        if (sum > highScore.second) {
+            highScore.first = iter->first;
+            highScore.second = sum;
+        }
     }
-    if(sum > highScore.second) {
-      highScore.first = iter.first;
-      highScore.second = sum;
-    }
-  }
-  return highScore.first;
+    return highScore.first;
 }
