3ba4363336c5de677e14b6bc37aa9f1313e3b7c8
nchalise219 <nchalise@ufl.edu>
2024-12-04 20:25:37 -0500

    Created while loop for UI to allow algorithm selection

---
+++ cmake-build-debug/CMakeFiles/Project_3_Group_67.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_3_Group_67.dir/main.cpp.obj"
+  "CMakeFiles/Project_3_Group_67.dir/main.cpp.obj.d"
+  "Project_3_Group_67.exe"
+  "Project_3_Group_67.exe.manifest"
+  "Project_3_Group_67.pdb"
+  "libProject_3_Group_67.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_3_Group_67.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
